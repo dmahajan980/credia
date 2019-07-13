c3afa4bdcaeddcde775074c123bdb18d2fd858d9
dmahajan980 <divyanshumahajan98@gmail.com>
2019-07-13 22:53:19 +0530

    Added form.

---
+++ homepage.js
@@ -0,0 +1,57 @@
+let validateNo = numberString => {
+    let number = parseInt(numberString);
+    if (Number.isInteger(number) && numberString.length == 10) {
+        document.querySelector(".headline-form").innerHTML = "One last thing!";
+        document.querySelector(".form-box").style.backgroundColor = "#C5DF16";
+        return true;
+    }
+    else {
+        document.querySelector(".form-box").style.backgroundColor = "rgb(189, 87, 87)";
+        document.querySelector(".headline-form").innerHTML = "Please enter a valid number";
+        return false;
+    }
+}
+
+let nextSlide = (parent, nextForm) => {
+    parent.classList.add('inactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+    nextForm.classList.remove('inactive');
+}
+
+let animate = () => {
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            if (validateNo(input.value)) {
+                console.log('good');
+                nextSlide(parent, nextForm);
+            }
+            else {
+                console.log('error');
+            }
+        })
+    })
+}
+
+let closeForm = () => {
+    let closeBtn = document.querySelector(".finish");
+    closeBtn.addEventListener('click', () => {
+        document.querySelector(".form-page").style.display = 'none';
+    })
+}
+
+let openForm = () => {
+    let formBtn = document.querySelector(".signup-button");
+    formBtn.addEventListener('click', () => {
+        document.querySelector(".form-page").style.display = 'flex';
+    })
+}
+
+animate();
+closeForm();
+openForm();
