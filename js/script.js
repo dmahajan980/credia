fe2ab242bae187381ec9079e8e9f4bc334ad781d
dmahajan980 <divyanshumahajan98@gmail.com>
2019-07-19 00:21:35 +0530

    Made changes to the form for taking mobile no as input and formatting it.

--- js/script.js
+++ js/script.js
@@ -1,62 +1,64 @@
+// Checks if number is vaid or invalid
 let validateNo = numberString => {
     let number = parseInt(numberString);
-    if (Number.isInteger(number) && numberString.length == 10) {
-        document.querySelector(".headline-form").innerHTML = "One last thing!";
-        document.querySelector(".form-box").style.backgroundColor = "#C5DF16";
-        return true;
-    }
-    else {
-        document.querySelector(".form-box").style.backgroundColor = "rgb(189, 87, 87)";
-        document.querySelector(".headline-form").innerHTML = "Please enter a valid number";
-        return false;
-    }
+    return (Number.isInteger(number) && numberString.length == 10) ? true : false;
 }
 
-let nextSlide = (parent, nextForm) => {
-    parent.classList.add('inactive');
-    parent.classList.remove('active');
-    nextForm.classList.add('active');
-    nextForm.classList.remove('inactive');
-}
-
-let animate = () => {
-    let counter = 0;
-    const arrows = document.querySelectorAll(".fa-arrow-down");
-    arrows.forEach(arrow => {
-        arrow.addEventListener('click', () => {
-            const input = arrow.previousElementSibling;
-            const parent = arrow.parentElement;
-            const nextForm = parent.nextElementSibling;
-
-            if (validateNo(input.value)) {
-                console.log('good');
-                nextSlide(parent, nextForm);
-                counter++;
-                if (counter === 2) {
-                    document.querySelector(".headline-form").innerHTML = 'Cheers!';
-                }
-            }
-            else {
-                console.log('error');
-            }
-        })
-    })
-}
-
-let closeForm = () => {
-    let closeBtn = document.querySelector(".finish");
-    closeBtn.addEventListener('click', () => {
-        document.querySelector(".form-page").style.display = 'none';
-    })
-}
-
-let openForm = () => {
-    let formBtn = document.querySelector(".signup-button");
-    setTimeout(formBtn.addEventListener('click', () => {
-        document.querySelector(".form-page").style.display = 'flex';
+// Takes the number string and displays popup
+let handleBtn = () => {
+    let btn = document.querySelector(".signup-button");
+    setTimeout(btn.addEventListener('click', () => {
+        // Pop message if nuumber is valid
+        // Else turn something red
+        animate();
     }), 500);
 }
 
-animate();
-closeForm();
-openForm();
+let popUp = () => {
+    let page = document.querySelector(".form-page");
+    page.style.display = 'flex';
+    setTimeout(() => {
+        page.style.display = 'none';
+        console.log("success!");
+    }, 2000);
+}
+
+const inputElement = document.querySelector(".number-field");
+
+// Formats the input inside textbox
+// Takes the input and converts it to a 10-digit sequence
+function animate() {
+
+    // Store number in the variable
+    let str = inputElement.value;
+    let formattedStr = str[6] + str[7] + str[8] + str[9] + str[11] + str[12] + str[13] + str[15] + str[16] + str[17];
+    
+    if (str.length == 18 && validateNo(formattedStr)) {
+        popUp();
+    }
+    else {
+        input.style.color = "rgb(255, 0, 0, 0.7)";
+        console.log("Invalid no!");
+    }
+}
+
+const formatToPhone = (event) => {
+
+    // I am lazy and don't like to type things more than once
+    const target = event.target;
+    // Strip everything except digits
+    // First twelve digits of input including 91
+    const input = target.value.replace(/\D/g,'').substring(0,12); 
+    const zip = "+91";
+    const start = input.substring(2, 6)
+    const middle = input.substring(6, 9);
+    const last = input.substring(9, 12);
+
+    if(input.length > 9){target.value = `${zip} - ${start} ${middle} ${last}`;}
+    else if(input.length > 6){target.value = `${zip} - ${start} ${middle}`;}
+    else if(input.length >= 0){target.value = `${zip} - ${start}`;}
+};
+
+inputElement.addEventListener('keypress', formatToPhone);
+
+handleBtn();
